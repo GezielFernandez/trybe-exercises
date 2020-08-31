1d73c829ba811c72175ef5d6474b4d14c5497132
Geziel Fernández <geziel.fernandez@pucp.pe>
2020-08-31 16:14:31 -0300

    primeiro exercício 4.3

---
+++ js-exercises/bloco_4/dia_3/4.3Exercises.js
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////////
+// Exercício 01: Imprimir um quadrado de asteriscos de dimensão n * n
+
+let n = 5;
+let ast = "";
+
+for(i = 0; i < n; i += 1) {
+    ast += "*";
+}
+for(i = 0; i < n; i += 1) {
+  console.log(ast);
+}
+
+
