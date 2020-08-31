e19b938fa231cbd4aad5c7c4f01358abd912234a
Geziel Fernández <geziel.fernandez@pucp.pe>
2020-08-31 19:28:55 -0300

    Second exercise made

--- js-exercises/bloco_4/dia_3/4.3Exercises.js
+++ js-exercises/bloco_4/dia_3/4.3Exercises.js
@@ -11,4 +11,17 @@ for(i = 0; i < n; i += 1) {
   console.log(ast);
 }
 
+//////////////////////////////////////////////////////////////
+// Exercício 02: Triângulo retângulo com 5 asteriscos de base
+let n = 5;
+let ast = "";
+
+for(i = 0; i < n; i += 1) {
+  ast += "*";
+  console.log(ast);
+}
+
+//////////////////////////////////////////////////////////////
+// Exercício 03:
+
 
