313ec8c156609fb1d2751f71b63b8042e5f77db4
Geziel Fernández <geziel.fernandez@pucp.pe>
2020-08-31 20:50:58 -0300

    third exercise, a proposal

--- js-exercises/bloco_4/dia_3/4.3Exercises.js
+++ js-exercises/bloco_4/dia_3/4.3Exercises.js
@@ -22,6 +22,18 @@ for(i = 0; i < n; i += 1) {
 }
 
 //////////////////////////////////////////////////////////////
-// Exercício 03:
+// Exercício 03: Invertindo o lado do triângulo (a proposal)
+let n = 5;
+let spaces = '';
+let ast = '';
 
+for(i = 0; i < n; i += 1) {
+  spaces += " ";  //Here we've got to let an space, kind of a phantom string!!
+  ast += '*';
+}
+
+for(i = 1; i <= n; i += 1) {
+  let spaceAst = spaces.slice(i) + ast.slice(n - i);
+  console.log(spaceAst);
+}
 
