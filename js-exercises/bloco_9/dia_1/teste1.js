7366d43a650ab269dac369d6c46e8bf3f77f7227
Geziel <geziel.fernandez@pucp.pe>
2020-10-21 15:55:55 -0300

    teste 1 de operaçao assincrona

---
+++ js-exercises/bloco_9/dia_1/teste1.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepEqual(numbers, [1, 2, 3]); // validação certa
+
+
+/// Com operação assíncrona e validação errada
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepEqual(numbers, [1, 2, 3]); // essa validação falha
+
+/// Operação assíncrona e validação adequada
+const assert = require('assert');
+
+const pushNumber = (list, number) => {
+  list.push(number);
+  console.log(list);
+};
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout(() => assert.deepEqual(numbers, [2, 3, 1]), 3000);
