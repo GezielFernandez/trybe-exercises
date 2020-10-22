a1d5163f198f793edc8af318e98072f0a461db1d
Geziel <geziel.fernandez@pucp.pe>
2020-10-22 07:27:49 -0300

    exercise 3

---
+++ js-exercises/bloco_9/dia_1/9.1exercise3.js
@@ -0,0 +1,14 @@
+// A função getPlanet abaixo imprime o planeta Marte de forma síncrona. Modifique getPlanet, de forma que Marte seja impresso assincronamente, depois de 4 segundos.
+// Answer: To do that, the console.log expression must be part of a setTimeout function, for insering the delay. 
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  setTimeout(() => console.log("Returned planet: ", mars), 4000);
+};
+
+getPlanet(); // imprime Marte depois de 4 segundos
